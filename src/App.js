e4f0d6af75208683c2e84eb22923fcd9c59a23ad
Tate Keller <tatek1993@gmail.com>
2020-02-10 17:23:03 -0600

    styling

--- src/App.js
+++ src/App.js
@@ -76,7 +76,7 @@ class App extends React.Component {
   // this component is going to take care of state, and any change handlers you need to work with your state
   render() {
     return (
-      <div>
+      <div className="items-container">
         <div className="header">
           <h2>Welcome to your Todo App!</h2>
           <ToDoForm addNewItem={this.addNewItem}/> 
